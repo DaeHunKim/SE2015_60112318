497c1cac327216ceb2b4dcdf3ec9d5db171e3ef1
DaeHunKim <eoqkfdl61022@naver.com>
2015-03-30 13:48:59 +0900

    김대후니가 무엇을 가르치나요

--- DI_0318_60112318/src/main/java/HacksaMain.java
+++ DI_0318_60112318/src/main/java/HacksaMain.java
@@ -4,7 +4,7 @@ public class HacksaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			Professor professor = new Professor("chol");
-			Lecture lecture1 = new Lecture("소프트웨어 공학");
+			Lecture lecture1 = new Lecture("C언어");
 			
 			ApplicationContext factory = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
 			
@@ -13,7 +13,7 @@ public class HacksaMain {
 			
 			professor.addLecture(lecture1);
 			professor.showLecture();
-			
+			System.out.println("김대훈 ㄴㄷㄹ");
 			
 	}
 
