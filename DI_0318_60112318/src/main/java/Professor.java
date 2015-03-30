97080d9e0c5f4cea8b99d3f0ce5a8b432578ffd7
DaeHunKim <eoqkfdl61022@naver.com>
2015-03-30 13:36:55 +0900

    Dependency Injection에 관한  수업 3월 30일

---
+++ DI_0318_60112318/src/main/java/Professor.java
@@ -0,0 +1,31 @@
+
+public class Professor {
+	private String name;
+	private Lecture lecture;
+	
+	public Professor(){
+		
+	}
+	
+	public Professor(String professorName) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void addLecture(Lecture lecture1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showLecture() {
+		// TODO Auto-generated method stub
+		
+	}
+}
